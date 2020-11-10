9fa7daad0ecc3a75e810dd0254a6fd79f4a06c3c
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-10 17:17:00 +0100

    jsnack 2 concluso

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -8,6 +8,8 @@
 // //Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array)
 // una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale
 
+var letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 var arrayObj = [
   { name: 'Poppy', type: 'tshirt', color: 'red' },
   { name: 'Jumping', type: 'occhiali', color: 'blue' },
@@ -15,3 +17,24 @@ var arrayObj = [
   { name: 'Jenny', type: 'borsa', color: 'pink' },
 ];
 console.log(arrayObj);
+
+var arrayObjTwo = [
+  { name: 'Poppy', type: 'tshirt', color: 'red' },
+  { name: 'Jumping', type: 'occhiali', color: 'blue' },
+  { name: 'CrissCross', type: 'scarpe', color: 'black' },
+  { name: 'Jenny', type: 'borsa', color: 'pink' },
+];
+
+// cliclo arrayObjTwo
+for (var i = 0; i < arrayObjTwo.length; i++) {
+  for (var key in arrayObjTwo[i]) {
+    arrayObjTwo[i].position = randomLetter();
+  }
+
+}
+console.log(arrayObjTwo);
+
+// funzione che genera lettera casuale
+function randomLetter(){
+  return letter.charAt(Math.floor(Math.random() * letter.length));
+}
