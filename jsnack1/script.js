33daadd9373bd2abb5735608acd43a439fa28673
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-10 16:43:20 +0100

    jsnack 1 completato

---
+++ jsnack1/script.js
@@ -0,0 +1,17 @@
+//Partendo da un array semplice tipo:var myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+// chiediamo all’utente con 2 prompt due numeri tra 0 e la lunghezza dell’array
+// quindi creiamone uno nuovo con solo i valori che hanno la posizione compresa tra i due numeri inseriti dall’utente
+
+myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+arrayUte = [];
+
+// INPUT
+var inputOne = parseInt(prompt("inserisci un numero tra 0 e 5"));
+var inputTwo = parseInt(prompt("inserisci un numero tra 0 e 5"));
+
+// creo il nuovo array
+for (var i = inputOne; i < inputTwo; i++) {
+  arrayUte.push(myArray[i])
+}
+
+console.log(arrayUte);
