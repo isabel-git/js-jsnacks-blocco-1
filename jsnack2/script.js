a939d3b69b704946f52921a1d306ab76af5b4c3a
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-10 16:48:49 +0100

    inizio jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -7,3 +7,11 @@
 // ];
 // //Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array)
 // una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale
+
+var arrayObj = [
+  { name: 'Poppy', type: 'tshirt', color: 'red' },
+  { name: 'Jumping', type: 'occhiali', color: 'blue' },
+  { name: 'CrissCross', type: 'scarpe', color: 'black' },
+  { name: 'Jenny', type: 'borsa', color: 'pink' },
+];
+console.log(arrayObj);
